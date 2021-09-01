eebda7c9da845b124fb46b27d593028480ceb66f
Anjali Gunduka <anjalig@nbos.vinisoft.com>
2021-09-01 15:18:56 +0530

    intial commit

---
+++ src/main/java/com/google/drive/App.java
@@ -0,0 +1,14 @@
+package com.google.drive;
+
+import org.apache.commons.text.WordUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println(WordUtils.capitalize("hello world!"));
+		System.out.println(WordUtils.capitalize("We are feature branch"));
+	}
+}
