95abe5d7bc6cefcb33584131970b799639d4844c
Anjali <anjaligunduka1997@gmail.com>
2021-09-02 18:17:07 +0530

    initial commit

---
+++ src/main/java/com/Calci/maths/Calculator.java
@@ -0,0 +1,21 @@
+package com.Calci.maths;
+
+public class Calculator {
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int Multi(int a, int b) {
+		return a * b;
+	}
+
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+	public static int div(int a, int b) {
+		return a / b;
+	}
+	public static double computeCircleRadius(double radius) {
+		return Math.PI * radius * radius;
+	}
+}
---
+++ src/test/java/com/Calci/maths/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.Calci.maths;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+	@Test
+	void testing()
+	{
+		System.out.println("Hi");
+	}
+}
